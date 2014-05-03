ae48b79861d4f9500ea6792f943444638575a92a
Benjamin Jordan <bmj465@gmail.com>
2014-05-03 11:52:46 -0700

    Lots of refactor

--- Main.js
+++ Main.js
@@ -14,30 +14,52 @@ window.requestAnimFrame = (function(){
         };
 })();
 
-var rect;
-var entries = [];
-var positions = [];
-var path;
-var paper;
+function Timeline(feedPath) {
+    this.rect;
+    this.entries = [];
+    this.positions = [];
+    this.links = [];
+    this.path;
+    this.paper;
 
-function initialize() {
-    this.feed = new google.feeds.Feed('http://thegoldenmule.svbtle.com/feed');
+    this.feed = new google.feeds.Feed(feedPath);
+
+    var that = this;
+    this.feed.setNumEntries(10000);
     this.feed.load(function(result) {
         if (!result.error) {
-            entries = result.feed.entries;
+            that.entries = result.feed.entries;
 
-            var width = 900;
+            var width = 1000;
             var height = 600;
-            paper = Raphael(0, 0, width, height);
-            rect = paper.rect(0, 0, width, height, 5).attr({fill:'#000', stroke:'none'});
+            var twidth = 50;
+            that.paper = Raphael('content', width, height);
+            that.rect = that.paper.rect(0, 0, width, height, 5).attr({fill:'#111', stroke:'none'});
+
+            // next button
+            that.paper
+                .path('M6.684,25.682L24.316,15.5L6.684,5.318V25.682z')
+                .attr({
+                    fill : '#FFF'
+                })
+                .transform('T' + (width - twidth) + ',' + (height - twidth))
+                .click(function(){
+                    for (var i = 0, len = links.length; i < len; i++) {
+                        var circle = links[i].circle;
+                        var title = links[i].title;
+
+                        circle.transform('t-10,0');
+                    }
+
+                    updatePath();
+                });
 
             var animateMS = 50;
             var circleRadius = 10;
             var attr = {font: '20px Helvetica', opacity: 0.5};
-            var twidth = 50;
-            for (var i = entries.length - 1; i >= 0; i--) {
+            for (var i = that.entries.length - 1; i >= 0; i--) {
                 var x = ((i + 1) * twidth);
-                var title = paper.text(0, 0, entries[i].title)
+                var title = that.paper.text(0, 0, that.entries[i].title)
                     .attr(attr)
                     .attr(
                     {
@@ -50,13 +72,18 @@ function initialize() {
                     x:x,
                     y:bbox.y2 + 4 * circleRadius
                 };
-                positions.push(position);
+                that.positions.push(position);
 
-                var circle = paper
+                var circle = that.paper
                     .circle(position.x, position.y, circleRadius)
                     .attr({
                         fill: '#FFF'
                     });
+
+                that.links.push({
+                    title:title,
+                    circle:circle
+                });
 
                 // handlers
                 var mouseOver = (function() {
@@ -78,7 +105,7 @@ function initialize() {
                 })();
 
                 var click = (function() {
-                    var entry = entries[i];
+                    var entry = that.entries[i];
                     return function (){
                         window.open(entry.link);
                     };
@@ -94,28 +121,33 @@ function initialize() {
                     .click(click);
             }
 
-            updatePath();
+            that.updatePath();
         }
     });
+}
 
-    function updatePath() {
-        if (path) {
-            path.remove();
-            path = null;
-        }
-
-        var pathString = 'M' + positions[0].x + ',' + positions[0].y + 'R';
-        for (var i = 1, len = positions.length; i < len; i++) {
-            pathString += ' ' + positions[i].x + ' ' + positions[i].y;
-        }
-
-        paper
-            .path(pathString)
-            .attr({
-                'stroke' : '#FFF',
-                'stroke-width': 4,
-                'stroke-linecap' : 'round'
-            })
-            .insertAfter(rect);
+Timeline.prototype.updatePath = function() {
+    if (this.path) {
+        this.path.remove();
+        this.path = null;
     }
+
+    var pathString = 'M' + this.positions[0].x + ',' + this.positions[0].y + 'R';
+    for (var i = 1, len = this.positions.length; i < len; i++) {
+        pathString += ' ' + this.positions[i].x + ' ' + this.positions[i].y;
+    }
+
+    this.path = this.paper
+        .path(pathString)
+        .attr({
+            'stroke' : '#FFF',
+            'stroke-width': 4,
+            'stroke-linecap' : 'round'
+        })
+        .insertAfter(this.rect);
+};
+
+function initialize() {
+    new Timeline('http://thegoldenmule.com/blog/feed/');
+    //new Timeline('http://thegoldenmule.svbtle.com/feed');
 }
